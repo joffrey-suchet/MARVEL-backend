9a2b84e614a5cfbb451ca1e9caa97d809ec7947d
joffrey-suchet <joffrey60s@gmail.com>
2022-08-04 16:05:37 +0200

    add-cles-title

--- routes/comics.js
+++ routes/comics.js
@@ -9,7 +9,7 @@ router.get("/comics", async (req, res) => {
   try {
     axios
       .get(
-        `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+        `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${req.query.title}`
       )
       .then((response) => {
         let comics = response.data;
